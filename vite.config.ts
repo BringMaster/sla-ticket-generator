f682222637ff9e929cb2bb20584659bb9a9e0c4d
BringMaster <7147312+BringMaster@users.noreply.github.com>
2025-08-28 14:28:15 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: true,
-    port: 8283,
+    port: 8080,
   },
   plugins: [
     react(),
